d9fa02c80687fe62f89a40199c38596233d2e233
kevin.tung <chen0625tung@gmail.com>
2021-04-13 15:46:08 +0800

    added query results page

--- index.js
+++ index.js
@@ -8,7 +8,15 @@ let client = null;
 
 app.get('/', (req, res) => {
     console.log('url hit at /');
-    res.send('<h1>Hello World!</h1>');
+    res.send(`
+        <h1>Punch Card API Server</h1>
+        <h2>routes</h2>
+        <ul>
+            <li>/</li>
+            <li>e.g. /write-time?year=2021&month=4&date=12&time=9:3:30&status=work</li>
+            <li>e.g. /get-monthly?month=4</li>
+        </ul>
+    `);
     res.end();
 });
 
@@ -27,7 +35,7 @@ app.get('/write-time', async (req, res) => {
         time, 
         status
     };
-    client = new MongoClient(dbUrl, { useUnifiedTopology: true, useNewUrlParser: true, connectTimeoutMS: 30000, keepAlive: 1 });
+    client = getDBinstance();
     client.connect(async err => {
         const collection = await client.db("pcard").collection("attendance");
         // perform actions on the collection object
@@ -49,4 +57,43 @@ app.get('/write-time', async (req, res) => {
     res.end();
 });
 
+// api to fetch all records
+
+// api to fetch monthly records
+app.get('/get-monthly', async (req, res) => {
+    const date = new Date();
+    const year = date.getFullYear();
+    const month = req.query.month;
+    console.log('url hit at get-monthly records...');
+    let pageData = '';
+
+    client = getDBinstance();
+    client.connect(async err => {
+        const collection = await client.db("pcard").collection("attendance");
+        const cursor = await collection.find({ "year": year.toString(), "month": month.toString() });
+        const data = await cursor.toArray();
+        const pageDataArray = data.map(e => {
+            return `${JSON.stringify(e)}, <br/>`;
+        });
+        pageData = pageDataArray.join('\n');
+        console.log(pageData);
+        res.send(`
+            <h1>Results for year ${year}, month ${month}</h1>
+            <div>
+                <ol>
+                    ${pageData.split(', ').map(e => `<li>${e}</li>`)}
+                </ol>
+            </div>
+        `);
+        console.log('done querying month data!');
+        res.end();
+        await client.close();
+    });
+});
+
 app.listen(process.env.PORT || PORT, () => console.log(`listening at port: ${PORT}`));
+
+
+function getDBinstance() {
+    return new MongoClient(dbUrl, { useUnifiedTopology: true, useNewUrlParser: true, connectTimeoutMS: 30000, keepAlive: 1 });
+}
